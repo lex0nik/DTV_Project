c7d85a4e28c9b0ddb3e766aa1a74a25394fd9051
Alex <lexsps@hotmail.com>
2018-12-26 22:26:22 +0200

    Update MessagesResponse.java
    small change

--- NewProject2/src/main/java/com/springboot/demo/model/MessagesResponse.java
+++ NewProject2/src/main/java/com/springboot/demo/model/MessagesResponse.java
@@ -7,28 +7,12 @@ public class MessagesResponse {
     private String status;
     private String message;
    
-    private String username;
+    
 
     public MessagesResponse(String status, String message) {
         this.status = status;
         this.message = message;
     }
-
-    public MessagesResponse(String status, String message, String role) {
-        this.status = status;
-        this.message = message;
-        
-    }
-
-   
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
 
 	public String getStatus() {
         return status;
@@ -48,7 +32,7 @@ public class MessagesResponse {
 
 	@Override
 	public String toString() {
-		return "MessagesResponse [status=" + status + ", message=" + message + ", username=" + username + "]";
+		return "MessagesResponse [status=" + status + ", message=" + message + "]";
 	}
 
    
